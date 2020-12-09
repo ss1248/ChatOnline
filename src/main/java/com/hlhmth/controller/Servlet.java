54fd88215bde9263b79a070fab45c8ac4a7ffc9d
ss1248 <1305041262@qq.com>
2020-12-09 22:24:17 +0800

    1

---
+++ src/main/java/com/hlhmth/controller/Servlet.java
@@ -0,0 +1,20 @@
+package com.hlhmth.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        response.setContentType("utf-8,charset=gb2312");
+        response.setCharacterEncoding("gb2312");
+        request.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("controller servlet用于接收用户请求，响应给客户端内容，重定向或转发");
+        out.println("JSP 用于展示数据和提供我们操作的请求");
+    }
+}
